772dd26cfd2bd09bdee362d83b81706cc8851bf3
Vivek D Patel <v0pate09@louisville.edu>
2021-09-07 14:11:59 -0400

    Changes in the Main program

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 	    System.out.println("Simple Java Program for Jenkins Demo!");
+	    System.out.println("Appending...");
+	    System.out.println("This is build using Jenkins CICD");
     }
 }
